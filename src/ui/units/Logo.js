117c926519cde39238b57a65e5f8a206139f6ec5
= <=>
2026-04-19 15:10:59 +0200

    chore: Ta bort en onödig log

--- src/ui/units/Logo.js
+++ src/ui/units/Logo.js
@@ -3,7 +3,6 @@ import logoSvg from '../assets/mittlogo.svg?raw';
 // Skapar logotypen dynamiskt, så att det anpassas till natt- respektive dagsläget
 export class Logo {
   constructor(logo) {
-    console.log(logo)
     logo.innerHTML = logoSvg;
   }
 }
